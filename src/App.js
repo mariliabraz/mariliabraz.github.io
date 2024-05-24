de5c8b2bbeba2434b686bcbfc621c5fb041b2779
mariliafranco <marilia.andrad@gmail.com>
2024-05-24 15:29:56 -0300

    update imports in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Header from "./components/Header";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import "./App.scss";
 
 function App() {
